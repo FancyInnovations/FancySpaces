60a7b04ef46c81746c98696d038067c02e7756c9
Oliver <oschi1303@gmail.com>
2025-12-08 18:49:00 +0100

    Refactor rate limit database to use ristretto cache

--- src/internal/ratelimit/database/memory/ratelimit_memory_database.go
+++ src/internal/ratelimit/database/memory/ratelimit_memory_database.go
@@ -1,58 +1,71 @@
 package memory
 
 import (
-	"sync"
+	"reflect"
 	"time"
+
+	"github.com/dgraph-io/ristretto/v2"
+)
+
+var (
+	ttl        = 15 * time.Minute
+	sizeOfTime = int64(reflect.TypeOf(time.Time{}).Size())
 )
 
 type DB struct {
-	tokens      map[string]int
-	refillTimes map[string]time.Time
-	tokensMu    sync.RWMutex
-	refillMu    sync.RWMutex
+	tokens      *ristretto.Cache[string, int]
+	refillTimes *ristretto.Cache[string, time.Time]
 }
 
 func NewDB() *DB {
+	tokens, err := ristretto.NewCache(&ristretto.Config[string, int]{
+		NumCounters: 500 * 10,         // x10 of expected number of elements when full
+		MaxCost:     16 * 1024 * 1024, // 16 MB
+		BufferItems: 64,               // keep 64
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	refillTimes, err := ristretto.NewCache(&ristretto.Config[string, time.Time]{
+		NumCounters: 500 * 10,         // x10 of expected number of elements when full
+		MaxCost:     16 * 1024 * 1024, // 16 MB
+		BufferItems: 64,               // keep 64
+	})
+	if err != nil {
+		panic(err)
+	}
+
 	return &DB{
-		tokens:      make(map[string]int),
-		refillTimes: make(map[string]time.Time),
+		tokens:      tokens,
+		refillTimes: refillTimes,
 	}
 }
 
 func (db *DB) GetTokens(client string) (int, error) {
-	db.tokensMu.RLock()
-	defer db.tokensMu.RUnlock()
-
-	tokens, exists := db.tokens[client]
+	count, exists := db.tokens.Get(client)
 	if !exists {
 		return 0, nil
 	}
-	return tokens, nil
+
+	return count, nil
 }
 
 func (db *DB) SetTokens(client string, tokens int) error {
-	db.tokensMu.Lock()
-	defer db.tokensMu.Unlock()
-
-	db.tokens[client] = tokens
+	db.tokens.SetWithTTL(client, tokens, 4, ttl)
 	return nil
 }
 
 func (db *DB) GetLastRefill(client string) (time.Time, error) {
-	db.refillMu.RLock()
-	defer db.refillMu.RUnlock()
-
-	t, exists := db.refillTimes[client]
+	t, exists := db.refillTimes.Get(client)
 	if !exists {
-		return time.Now().Add((-5) * time.Minute), nil
+		return time.Now().Add((-5) * time.Minute), nil // return a time in the past to trigger immediate refill
 	}
+
 	return t, nil
 }
 
 func (db *DB) SetLastRefill(client string, t time.Time) error {
-	db.refillMu.Lock()
-	defer db.refillMu.Unlock()
-
-	db.refillTimes[client] = t
+	db.refillTimes.SetWithTTL(client, t, sizeOfTime, ttl)
 	return nil
 }
