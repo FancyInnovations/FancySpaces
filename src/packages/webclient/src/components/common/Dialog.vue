8fd4f7c9b4e8f787f06d1f8fd7463f903e046b92
Oliver <oschi1303@gmail.com>
2026-01-26 20:51:17 +0100

    website: Add dialog component

---
+++ src/packages/webclient/src/components/common/Dialog.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+
+const props = defineProps<{
+  shown: boolean;
+  persistent?: boolean;
+  width?: string;
+  height?: string;
+}>();
+
+</script>
+
+<template>
+  <v-dialog
+    v-model="props.shown"
+    :height="props.height ?? 'max-content'"
+    :persistent="props.persistent ?? false"
+    :width="props.width ?? 'max-content'"
+    opacity="0.6"
+    transition="dialog-transition"
+  >
+    <div class="dialog__background">
+      <slot></slot>
+    </div>
+  </v-dialog>
+</template>
+
+
+<style scoped>
+.dialog__background {
+  height: 100%;
+}
+
+.dialog__background > * {
+  background-color: rgba(21, 13, 25, 0.6) !important;
+  backdrop-filter: blur(10px);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+}
+</style>
