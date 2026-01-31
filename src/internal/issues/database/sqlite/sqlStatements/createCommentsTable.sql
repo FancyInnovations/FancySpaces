367d8f4f0bd8997c82cd9dc3873006b9697e81e6
Oliver <oschi1303@gmail.com>
2026-01-31 20:33:45 +0100

    Remove sqlite implementation for issues

--- src/internal/issues/database/sqlite/sqlStatements/createCommentsTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE issue_comments (
-    id TEXT PRIMARY KEY,
-    issue TEXT NOT NULL,
-    author TEXT NOT NULL,
-    content TEXT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
-    FOREIGN KEY (issue) REFERENCES issues(id) ON DELETE CASCADE
-);
