7f5d5e2f946132575a1f3a009327bc4a2c11009f
Oliver <oschi1303@gmail.com>
2026-02-03 23:48:13 +0100

    Setup storage service

---
+++ services/storage/cmd/local/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/OliverSchlueter/goutils/middleware"
+	"github.com/OliverSchlueter/goutils/sloki"
+	"github.com/fancyinnovations/fancyspaces/storage/internal/app"
+	"github.com/justinas/alice"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	// Setup logging
+	logService := sloki.NewService(sloki.Configuration{
+		URL:          "http://localhost:3100/loki/api/v1/push",
+		Service:      "fancyspaces-storage",
+		ConsoleLevel: slog.LevelDebug,
+		LokiLevel:    slog.LevelInfo,
+		EnableLoki:   false,
+		Handlers:     []sloki.LogHandler{},
+	})
+	slog.SetDefault(slog.New(logService))
+
+	// Setup HTTP server
+	port := "8090"
+	mux := http.NewServeMux()
+
+	app.Start(app.Configuration{
+		Mux: mux,
+	})
+
+	chain := alice.New(
+		middleware.RequestLogging,
+		//auth.Middleware,
+		middleware.Recovery,
+	).Then(mux)
+
+	go func() {
+		err := http.ListenAndServe(":"+port, chain)
+		if err != nil {
+			slog.Error("Could not start server on port "+port, sloki.WrapError(err))
+			os.Exit(1)
+		}
+	}()
+
+	slog.Info(fmt.Sprintf("Started server on http://localhost:%s\n", port))
+
+	// Wait for a signal to exit
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	switch <-sig {
+	case os.Interrupt:
+		slog.Info("Received interrupt signal, shutting down...")
+
+		slog.Info("Shutdown complete")
+	}
+}
