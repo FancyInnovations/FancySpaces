f04648d5b27d905b94a2cd68232f7310ede16fce
Oliver <oschi1303@gmail.com>
2026-02-17 21:05:41 +0100

    idp-go-sdk: Use Argon2 for password hashes

--- integrations/idp-go-sdk/idp/hash.go
+++ integrations/idp-go-sdk/idp/hash.go
@@ -1,13 +1,24 @@
 package idp
 
 import (
-	"crypto/sha256"
-	"fmt"
+	"crypto/rand"
+	"encoding/base64"
+
+	"golang.org/x/crypto/argon2"
 )
 
+// PasswordHash generates a secure hash for the given password using the Argon2 algorithm.
 func PasswordHash(password string) string {
-	h := sha256.New()
-	h.Write([]byte(password))
-	bs := h.Sum(nil)
-	return fmt.Sprintf("%x", bs)
+	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
+	if err != nil {
+		return ""
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, 3, 64*1024, 4, 32)
+
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	return b64Salt + "." + b64Hash
 }
